bd9e098fc758568438b184ce1e6aa93726cd7dcf
yusame <yu.starts.yu@gmail.com>
2023-05-26 16:10:36 +0900

    GitHubRepo Entityを追加

---
+++ GitHubRepo-Combine-UIKit/Entity/GitHubRepo.swift
@@ -0,0 +1,36 @@
+//
+//  GitHubRepo.swift
+//  GitHubRepo-Combine-UIKit
+//
+//  Created by 小原宙 on 2023/05/26.
+//
+
+struct GitHubRepo: Codable {
+    let fullName: String
+    let stargazersCount: Int
+    let htmlUrl: String
+    let owner: GitHubRepoOwner
+    
+    enum CodingKeys: String, CodingKey {
+        case fullName = "full_name"
+        case stargazersCount = "stargazers_count"
+        case htmlUrl = "html_url"
+        case owner
+    }
+    
+    var stargazerText: String {
+        "⭐︎ " + String(stargazersCount)
+    }
+}
+
+struct GitHubRepoOwner: Codable {
+    let avatarUrl: String
+    
+    enum CodingKeys: String, CodingKey {
+        case avatarUrl = "avatar_url"
+    }
+}
+
+struct GitHubRepoList: Codable {
+    let items: [GitHubRepo]
+}
